65d0115a20dde3376b1300ebcbf7a1e66acdba80
JustinAmstadt <justinamstadt@gmail.com>
2024-04-14 00:04:02 -0700

    successfully mints

--- src/monster.ts
+++ src/monster.ts
@@ -2,43 +2,65 @@ import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
 import { MIST_PER_SUI } from '@mysten/sui.js/utils';
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { Ed25519Keypair, Ed25519PublicKey } from '@mysten/sui.js/keypairs/ed25519';
+import { SuiTransactionBlockResponse } from '@mysten/sui.js/client';
 import { fromB64 } from '@mysten/bcs';
 import dotenv from 'dotenv';
+import { send } from 'process';
+
+const PACKAGE_ID = "0x9cd22220447b3076c5c7ade08206ed64d0e742f09e83e8389e39aabbdda60ec3";
+
+function getKeypairFromPrivateKey(key: string) {
+    const raw = fromB64(key);
+    return Ed25519Keypair.fromSecretKey(raw.slice(1));
+}
+
+async function monster_mint_to_sender(
+        suiClient: SuiClient, 
+        keypair: Ed25519Keypair, 
+        gasPrice: number, 
+        gasBudget: number, 
+        name: string, 
+        description: string, 
+        cry: string) : Promise<SuiTransactionBlockResponse> {
+    const pk = keypair.getPublicKey();
+    const sender = pk.toSuiAddress();
+
+    const txb = new TransactionBlock();
+    txb.setSender(sender);
+    txb.setGasPrice(gasPrice);
+	txb.setGasBudget(gasBudget);
+
+    txb.moveCall({
+        target: `${PACKAGE_ID}::monster::mint_to_sender`,
+        arguments: [txb.pure.string(name), txb.pure.string(description), txb.pure.string(cry)],
+    });
+
+    const bytes = await txb.build({ client: suiClient });
+    const serializedSignature = (await keypair.signTransactionBlock(bytes)).signature;
+
+    keypair.signTransactionBlock(bytes);
+    const response = suiClient.executeTransactionBlock({
+        transactionBlock: bytes,
+        signature: serializedSignature
+    })
+
+    return response;
+}
 
 async function main() {
     dotenv.config();
 
-    const MY_ADDRESS: string = '0x7e9c3af8f5a75290d10c2dea3004d4928a105e7542240d1cacdfef3899366da1';
+    const secretKey: string = process.env.SECRET_KEY!;
+    const keypair: Ed25519Keypair = getKeypairFromPrivateKey(secretKey);
 
     const suiClient: SuiClient = new SuiClient({ url: getFullnodeUrl('testnet') });
+    let response = await suiClient.getOwnedObjects({ owner: keypair.getPublicKey().toSuiAddress() })
+    response.data.forEach(obj => console.log(obj));
 
-    const mistToSui = (balance: string): number => {
-        return parseInt(balance) / Number(MIST_PER_SUI);
-    };
+    let response1 = await suiClient.getObject({ id: "0xcef621c7cc6275a1fc918ef78c24f1a16e59025c68e992354353e82c136ec4e0"})  
+    console.log(response1)
 
-    const balance = await suiClient.getBalance({
-        owner: MY_ADDRESS,
-    });
-
-    console.log(`Balance: ${mistToSui(balance.totalBalance)}`)
-    const secretKey: string = process.env.SECRET_KEY!;
-    const raw = fromB64(secretKey);
-    const keypair = Ed25519Keypair.fromSecretKey(raw.slice(1));
-    console.log(keypair.getSecretKey())
-
-    const txb = new TransactionBlock();
-
-    txb.moveCall({
-        target: '0x79c6d95073d0f09a0151ae30643c325488ad41d7440a23cb8b32fdf3b761baad::monster::mint_to_sender',
-        arguments: [txb.pure.string('Justin\'s Monster'), txb.pure.string('Justin\'s Description'), txb.pure.string('Justin\'s Cry')],
-    });
-
-    const bytes = await txb.build({ client: suiClient });
-
-
-    keypair.signTransactionBlock(bytes);
-    suiClient.executeTransactionBlock
-
+    // await monster_mint_to_sender(suiClient, keypair, 1000, 100000000);
 }
 
 main();
